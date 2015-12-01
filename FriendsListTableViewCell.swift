31ee3f3724d198ca0373a5aa1a1a561443b5d2d9
Weiqi Wei <weiqi.wei@duke.edu>
2015-11-30 23:25:52 -0500

    import ios-charts

---
+++ FriendsListTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FriendsListTableViewCell.swift
+//  Physao
+//
+//  Created by Weiqi Wei on 15/11/26.
+//  Copyright © 2015年 Physaologists. All rights reserved.
+//
+
+import Cocoa
+
+class FriendsListTableViewCell: UITableViewCell {
+
+}
